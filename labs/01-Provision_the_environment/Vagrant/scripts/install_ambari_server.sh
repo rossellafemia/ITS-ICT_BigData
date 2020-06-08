3ef16c00af085f7e1a4691d7b4d4be4e547b1acb
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-06-09 00:31:02 +0200

    setup cluster

--- labs/01-Provision_the_environment/Vagrant/scripts/install_ambari_server.sh
+++ labs/01-Provision_the_environment/Vagrant/scripts/install_ambari_server.sh
@@ -5,7 +5,8 @@ setenforce 0
 yum -y install wget
 
 # Download Ambari Repository
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+#wget -nv https://archive.cloudera.com/p/ambari/centos7/2.x/updates/2.7.5.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.4.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
 
 # Install java-1.8
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
