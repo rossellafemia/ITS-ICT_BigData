754d1ed6822407f7e38237ec60a18533ddfc655b
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-07-15 23:13:46 +0200

    fix pig install

--- labs/02-Provision_the_environment/Vagrant/scripts/install_pig_0.17.sh
+++ labs/02-Provision_the_environment/Vagrant/scripts/install_pig_0.17.sh
@@ -6,7 +6,7 @@ cd /opt
 sudo tar xzf pig-0.17.0.tar.gz
 
 
-echo "export PIG_HOME=/opt/pig/pig-0.17.0" >> /home/vagrant/.bash_profile
+echo "export PIG_HOME=/opt/pig-0.17.0" >> /home/vagrant/.bash_profile
 echo "export PATH=\$PATH:\$PIG_HOME/bin" >> /home/vagrant/.bash_profile
 echo "export PIG_CLASSPATH=\"/etc/hadoop/+([0-9]).+([0-9]).+([0-9]).+([0-9])-+([0-9])/0\"" >> /home/vagrant/.bash_profile
 echo "export JAVA_HOME=/usr" >> /home/vagrant/.bash_profile
