8f8697bd0270285018155925cd206820dcb29fe2
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-07-17 12:40:43 +0200

    fix

--- labs/02-Provision_the_environment/Vagrant/scripts/install_maven.sh
+++ labs/02-Provision_the_environment/Vagrant/scripts/install_maven.sh
@@ -3,3 +3,4 @@ curl https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.
 unzip apache-maven-3.6.3-bin.zip 
 sudo cp -rf apache-maven-3.6.3 /opt/
 echo "export PATH=\$PATH:/opt/apache-maven-3.6.3/bin" >> /home/vagrant/.bash_profile
+rm -rf apache-maven-3.6.3 apache-maven-3.6.3-bin.zip 
