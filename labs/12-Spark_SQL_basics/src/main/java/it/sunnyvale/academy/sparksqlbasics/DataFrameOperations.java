e0d7faf11bde797b463157acf36c39107076cc48
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-11-19 18:53:57 +0100

    DataFrame operations using Lambda Expressions

--- labs/12-Spark_SQL_basics/src/main/java/it/sunnyvale/academy/sparksqlbasics/DataFrameOperations.java
+++ labs/12-Spark_SQL_basics/src/main/java/it/sunnyvale/academy/sparksqlbasics/DataFrameOperations.java
@@ -35,43 +35,71 @@ public class DataFrameOperations {
                 .option("multiline", "true")
                 .json("lab12_input"+System.getProperty("file.separator")+"users.json");
 
-        // print the JSON schema
+        // Print the JSON schema
+        System.out.println("=== Print the JSON schema ===");
         df.printSchema();
 
         // Show all
+        System.out.println("=== Show all ===");
         df.show();
 
         // Select only the "firstName" column
+        System.out.println("=== Select only firstName column ===");
         df
                 .select("firstName")
                 .show();
 
         // Select everybody (firstName and age)
+        System.out.println("=== Select only firstName and age columns ===");
         df
                 .select(col("firstName"), col("age"))
                 .show();
 
         // Select people older than 21
+        System.out.println("=== Select people older than 21 ===");
         df
                 .filter(col("age").gt(21))
                 .show();
 
-        // filtering using Expressions
+        // Select people older than 21 (Lambda)
+        System.out.println("=== Select people older than 21 (Lambda) ===");
+        df
+                .filter((FilterFunction<Row>) row -> ((Long)row.getAs("age")) > 21)
+                .show();
+
+        // Select people younger than 21 using Expressions
+        System.out.println("=== Select people younger than 21 using Expressions ===");
         df
                 .filter("age < 21")
                 .show();
 
-        // filtering with Lambdas
+        // Select people younger than 21 using Lambda
+        System.out.println("=== Select people younger than 21 using Lambda ===");
+        df
+                .filter((FilterFunction<Row>) row -> ((Long)row.getAs("age")) < 21)
+                .show();
+
+        // Select people with name starting with L using Lambda
+        System.out.println("=== Select people younger than 21 using Lambda ===");
         df
                 .filter((FilterFunction<Row>) row -> row
                                 .getAs("firstName")
                                 .toString().startsWith("L"))
                 .show();
 
-        // columns
+        // Select people with green eye
+        System.out.println("=== Select people with green eye ===");
         Column eyeColourColumn = df.col("eyeColor");
         df
                 .filter(eyeColourColumn.geq("green"))
+                .show();
+
+        // columns
+        System.out.println("=== Select people with green eye (Lambda) ===");
+        df
+                .filter((FilterFunction<Row>) row -> row
+                        .getAs("eyeColor")
+                        .equals("green"))
                 .show();
 
     }
