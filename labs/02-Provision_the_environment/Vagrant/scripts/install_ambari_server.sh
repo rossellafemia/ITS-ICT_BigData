7eebba673a6fc560ae2f4bcff90ffe6a25305a58
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2021-05-28 00:49:26 +0200

    Ambari from Make Open Source Great Again ;-)

--- labs/02-Provision_the_environment/Vagrant/scripts/install_ambari_server.sh
+++ labs/02-Provision_the_environment/Vagrant/scripts/install_ambari_server.sh
@@ -7,13 +7,18 @@ sudo yum install mysql-connector-java -y
 
 # Download Ambari Repository
 #wget -nv https://archive.cloudera.com/p/ambari/centos7/2.x/updates/2.7.5.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.4.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+#wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.4.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget -O /etc/yum.repos.d/mosga.repo https://makeopensourcegreatagain.com/repos/centos/7/ambari/2.7.5.0/mosga-ambari.repo
+
 
 # Install java-1.8
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
 # Install ambari-server
 yum -y install ambari-server
+
+# Install Management Pack - HDP 3.1.4.0
+ambari-server install-mpack --mpack=https://github.com/steven-matison/dfhz_hdp_mpack/raw/master/hdp-ambari-mpack-3.1.4.0.tar.gz --verbose
 
 # bootstrap ambari-server
 ambari-server setup -s --java-home=/usr/lib/jvm/jre/
