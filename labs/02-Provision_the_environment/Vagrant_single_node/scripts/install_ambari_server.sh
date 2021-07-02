44fbbd6fba771fdbd0f06e177a875d18177b5ef4
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2021-07-02 17:22:26 +0000

    fix makeopensourcegreatagain.com invalid cert

--- labs/02-Provision_the_environment/Vagrant_single_node/scripts/install_ambari_server.sh
+++ labs/02-Provision_the_environment/Vagrant_single_node/scripts/install_ambari_server.sh
@@ -7,7 +7,7 @@ sudo yum install mysql-connector-java -y
 
 # Download Ambari Repository
 #wget -nv https://archive.cloudera.com/p/ambari/centos7/2.x/updates/2.7.5.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.4.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget --no-check-certificate -O /etc/yum.repos.d/mosga.repo https://makeopensourcegreatagain.com/repos/centos/7/ambari/2.7.5.0/mosga-ambari.repo
 
 # Install java-1.8
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
